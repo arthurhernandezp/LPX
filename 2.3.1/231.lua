dcb4907812233a42b75f98fd22789642f65a89dd
Arthur Hernandez <57412723+arthurhernandezp@users.noreply.github.com>
2023-07-18 11:52:23 -0300

    Create 231.lua

---
+++ 2.3.1/231.lua
@@ -0,0 +1,13 @@
+function F()
+    f = io.open("teste.txt", "r") 
+    letra = f:read(1)
+    while letra do
+        coroutine.yield(letra)
+        letra= f:read(1)
+    end
+    f:close()
+end
+
+for c in coroutine.wrap(F) do
+    print(c)
+end
